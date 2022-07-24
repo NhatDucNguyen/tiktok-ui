cec66de035d0fd0858c2f04888cbcd7c54da2093
NhatDucNguyen <ducnhat220699@gmail.com>
2022-07-25 04:24:36 +0700

    Xây dựng UI phần Header 1

---
+++ src/assets/icons/index.js
@@ -0,0 +1,7 @@
+const icons = {
+    search: require('~/assets/icons/search.svg').default,
+    close: require('~/assets/icons/close.svg').default,
+    loading: require('~/assets/icons/loading.svg').default,
+};
+
+export default icons;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,37 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 
+import images from '~/assets/images';
+import icons from '~/assets/icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        {/* <img src={icons.close} alt="Close" /> */}
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    {/* <img src={icons.loading} alt="Loading" /> */}
+                    <span className={cx('divider')}></span>
+                    <button className={cx('search-btn')}>
+                        {/* <img src={icons.search} alt="Search" /> */}
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 }
